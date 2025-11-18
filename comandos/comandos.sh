740f6c7412a4981966a3083d2ba6f94c8bec7770
João Almeida <joao08almeida07@gmail.com>
2025-11-17 22:11:21 -0300

    Atualização

---
+++ comandos/comandos.sh
@@ -0,0 +1,94 @@
+ 1  sudo apt install curl
+    2  123@senac
+    3  sudo apt install curl
+    4  sudo apt install cur
+    5  sudo dpkg --configure -a
+    6  sudo apt install curl
+    7  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    8  sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+    9  sudo apt update
+   10  sudo apt install brave-browser
+   11  sudo snap install chromium
+   12  sudo rm /etc/apt/preferences.d/nosnap.pref
+   13  sudo apt update
+   14  sudo snap install chromium
+   15  sudo apt update
+   16  sudo apt install chromium
+   17  flatpak install flathub
+   18  flatpak run
+   19  com.guthub.micahflee.torbrowser-laucher
+   20  flatpak run com.github.micahflee.torbrowser-launcher
+   21  ~flatpak run com.github.micahflee.torbrowser-launcher
+   22  flatpak run com.github.micahflee.torbrowser-launcher
+   23  flatpak run
+   24  flatpak install flathub com.github.micahflee.torbrowser-launcher
+   25  flatpak install flathub org.torproject.torbrowser-launcher
+   26  flatpak run org.torproject.torbrowser-launcher
+   27  flatpak run org.torproject.torbrowser-launcher
+   28  flatpak run org.torproject.torbrowser-launcher
+   29  oing -c 4 google.com
+   30  ping google.com
+   31  flatpak run com.github.micahflee.torbrowser-launcher
+   32  flatpak install flathub com.github.micahflee.torbrowser-launcher
+   33  flatpak run com.github.micahflee.torbrowser-launcher
+   34  flatpak run org.torproject.torbrowser-launcher
+   35  flatpak install flathub org.torproject.torbrowser-launcher
+   36  flatpak list | grep tor
+   37  flatpak install flathub org.torproject.torbrowser-launcher
+   38  flatpak run org.torproject.torbrowser-launcher//stable
+   39  flatpak run org.torproject.torbrowser-launcher.desktop
+   40  ping -c 4 google.com
+   41  ping -c 4 google.com
+   42  flatpak run org.torproject.torbrowser-launcher --verbose
+   43  flatpak run org.torproject.torbrowser-launcher
+   44  cd ~/Downloads
+   45  wget https://www.torproject.org/dist/torbrowser/14.0.3/tor-browser-linux-x86_64-14.0.3.tar.xz
+   46  tar -xf tor-browser-linux-*.tar.xz
+   47  cd tor-browser
+   48  ./start-tor-browser
+   49  tar -xf tor-browser-linux-*.tar.xz
+   50  cd tor-browser
+   51  ./start-tor-browser
+   52  tar -xf tor-browser-linux-*.tar.xz
+   53  cd tor-browser
+   54  ./start-tor-browser
+   55  tar -xf tor-browser-linux-*.tar.xz
+   56  cd tor-browser
+   57  ./start-tor-browser 
+   58  wget https://www.torproject.org/dist/torbrowser/14.0.3/tor-browser-linux-x86_64-14.0.3.tar.xz
+   59  tar -xf tor-browser-linux-x86_64-14.0.3.tar.xz
+   60  cd tor-browser
+   61  ./start-tor-browser
+   62  cd ~/Downloads
+   63  wget https://www.torproject.org/dist/torbrowser/14.0.3/tor-browser-linux-x86_64-14.0.3.tar.xz
+   64  tar -xf tor-browser-linux-x86_64-14.0.3.tar.xz
+   65  ls
+   66  1s
+   67  11s1
+   68  1
+   69  sudo
+   70  sudo -i
+   71  sudo apt
+   72  sudo apt up   
+   73  lsudo apt update
+   74  sudo apt update
+   75  sudo apt upgrade
+   76  sudo dpkg --configure -a
+   77  sudo apt upgrade
+   78  sudo apt dist-upgrade
+   79  sudo apt full-upgrade
+   80  sudo apt autoremove
+   81  sudo apt autoclean
+   82  sudo apt clean
+   83  sudo apt update
+   84  apt list --upgradable
+   85  -a
+   86  sudo apt install git vim pip python 
+   87  sudo apt install git vim pip python
+   88  apt list --upgradable
+   89  sudo apt install git vim pip python
+   90  apt list --upgrade 
+   91  sudo apr install git vim python3 pip
+   92  sudo apt update
+   93  sudo apt install git vim python3 pip
+   94  history
